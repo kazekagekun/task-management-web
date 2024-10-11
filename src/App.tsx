9cb937d5d94c0f4cf04f2f8c693693fc9c1ed855
Hafiz <kazekagekun94@gmail.com>
2024-10-11 23:21:50 +0800

    Add mantine provider and theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,33 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import '@mantine/core/styles.css';
+import { Button, Card, createTheme, MantineColorsTuple, MantineProvider } from '@mantine/core';
+
+const myColor: MantineColorsTuple = [
+  '#e6ffee',
+  '#d3f9e0',
+  '#a8f2c0',
+  '#7aea9f',
+  '#54e382',
+  '#3bdf70',
+  '#2bdd66',
+  '#1bc455',
+  '#0bae4a',
+  '#00973c'
+];
+
+const theme = createTheme({
+  colors: {
+    myColor,
+  }
+});
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <MantineProvider theme={theme}>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -17,19 +38,19 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <Card>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
-      <p className="read-the-docs">
+      </Card>
+      <p>
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  );
+      </MantineProvider>  
+    );
 }
 
 export default App;
