5639e20a3415a42ac75e573c106b7454506b201a
Hafiz <kazekagekun94@gmail.com>
2024-10-12 19:28:19 +0800

    Chore: remove unecessary api calls

--- src/features/tasks/api/AddTask.ts
+++ src/features/tasks/api/AddTask.ts
@@ -11,20 +11,13 @@ const addTask = async (
   return response.data;
 };
 
-// Custom hook for adding a task
 export const useAddTask = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: addTask,
-    onSuccess: (newTask) => {
-      // Invalidate and refetch
+    onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['tasks'] });
-
-      // Optionally, you can update the cache directly
-      queryClient.setQueryData<Task[]>(['tasks'], (oldTasks) => {
-        return oldTasks ? [...oldTasks, newTask] : [newTask];
-      });
     },
   });
 };
--- src/features/tasks/api/UpdateTask.ts
+++ src/features/tasks/api/UpdateTask.ts
@@ -4,32 +4,20 @@ import { Task } from '../interface';
 import { AddTaskSchema } from '../schema';
 import { z } from 'zod';
 
-// Define a type for the update task payload
 type UpdateTaskPayload = z.infer<typeof AddTaskSchema> & { id: string };
 
-// Function to update a task
 const updateTask = async (updatedTask: UpdateTaskPayload): Promise<Task> => {
   const response = await api.put(`/tasks/${updatedTask.id}`, updatedTask);
   return response.data;
 };
 
-// Custom hook for updating a task
 export const useUpdateTask = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: updateTask,
-    onSuccess: (updatedTask) => {
-      // Invalidate and refetch
+    onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['tasks'] });
-
-      // Optionally, you can update the cache directly
-      queryClient.setQueryData<Task[]>(['tasks'], (oldTasks) => {
-        if (!oldTasks) return [updatedTask];
-        return oldTasks.map((task) =>
-          task.id === updatedTask.id ? updatedTask : task,
-        );
-      });
     },
   });
 };
